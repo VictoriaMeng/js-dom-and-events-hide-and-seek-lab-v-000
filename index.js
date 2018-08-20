5bb44ef1c2b9d3a359cb316bffe8188408b62c96
Victoria Meng <nights1ststar@protonmail.com>
2018-08-20 01:45:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelectorAll(selector)[0];
+};
+function nestedTarget(){
+  return document.getElementById('nested');
+};
+
+function deepestChild(){
+  let selector = '#grand-node div';
+  const list = document.querySelectorAll(selector);
+  for (let i = 1; i < list.length; i++) {
+    selector = selector + ' div';
+  };
+  return document.querySelector(selector);
+};
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('ul.ranked-list');
+  for (let i of list) {
+    i.innerHTML = parseInt(i.innerHTML) + parseInt(n);
+  };
+};
