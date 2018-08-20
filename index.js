6114861b906a87803e7413d2126b93b3263b6c64
Victoria Meng <nights1ststar@protonmail.com>
2018-08-20 01:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested');
 };
+
+function deepestChild(){
+  let children = document.getElementById('grand-node');
+  while (!!children[0]) {
+    children = children[0];
+  }
+  return children;
+};
+
 function increaseRankBy(n){
   return document.querySelectorAll('.ranked-list') + n;
 }
